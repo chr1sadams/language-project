09cf40b0d5958d170a8dca8d847c03dc75400dc4
mc12elis <mc12elis@siena.edu>
2019-11-29 19:41:17 -0500

    Added comments and structure to tweet headlines through intervals

--- test4.js
+++ test4.js
@@ -2,6 +2,14 @@ const Nylas = require("nylas");
 const fs = require('fs');
 const BitlyClient = require('bitly').BitlyClient;
 const bitly = new BitlyClient("75712e9e08c53b867bc225a9d1a358ff99d22642");
+const Twit = require('twit');
+
+const T = new Twit({
+    consumer_key: "W1flukTtPNG8ntlRo5w4i4DyE",
+    consumer_secret: "0iV8ruZ2RDrhyeWAuTw9keHQKrX1q4MbfgFesg4I3nizZbIXHr",
+    access_token: "1194422744550694912-436rX24oOIiDlpmrqWuxu53d313Ma0",
+    access_token_secret: "GN3BJCex8xfpsIx5uKUoj06gpGIDKD9hzZy8ns1MUHT71",
+});
 
 Nylas.config({
     clientId: "9vzuxd9fvv6eddr7zib1x2jen",
@@ -11,16 +19,25 @@ Nylas.config({
 const nylas = Nylas.with("vwTE8AbNrQ8l9xfQVANHAUnS5a5cuw");
 
 async function tweetFormatter() {
+    
+    //collect message from email containing all headline information
     var messages = await nylas.messages.list({in: 'inbox', from: 'student-digest@siena.edu', limit: 6});
+    
+    //initialize variables
     var dates = [];
     var partitions = [];
     var categories = [];
+
     for (let message of messages) {
         dates.push(message.date);
         if (dates.length == 5) {
+            //create a variable containing message contents in html
             var messageBody = message.body;
+
+            //trim string to focus on the headline content.
             messageBody = messageBody.substring(messageBody.indexOf("Today's News"), messageBody.indexOf("In Case You Missed It"));
             
+            //loop to split html into separate partitions of categories.
             var index = 1;
             var endIndex = 1;
             while (endIndex != -1) {
@@ -33,6 +50,8 @@ async function tweetFormatter() {
                     partitions.push(messageBody.substring(index));
                 }
             }
+
+            //scan each partition for necessary headline information
             for (var i = 0; i < partitions.length; i++) {
                 categories[i] = [];
                 var str = partitions[i];
@@ -40,18 +59,33 @@ async function tweetFormatter() {
                 str = str.substring(str.indexOf("</h3>") + 5);
                 console.log(categories[i][0] + '\n');
 
+                //take headline information and splice it neatly into a tweet format. store into array afterwards.
                 endIndex = 0;
                 while (endIndex != -1) {
+
+                    //init headline variable to hold formatted tweet.
                     var headline = '';
+
+                    //scan the headline name and concat it to the headline variable
                     headline = headline + str.substring(str.indexOf('/">') + 3, str.indexOf("</a>")) + '\n';
+                    
+                    //separate large html string to scan the headline description and concat to headline variable
                     var desc = str.substring(str.indexOf("</font>") + 7);
                     headline = headline + desc.substring(desc.indexOf("'black'>") + 8, desc.indexOf("</font>"));
+                    
+                    //scan original html string to get authors name and concat to headline variable
                     headline = headline + str.substring(str.indexOf("'black'>") + 8, str.indexOf("</font>")) + '\n';
+                    
+                    //scan hyperlink to article and format into bit.ly hyperlink for shortened characters.
                     var hyperlink = str.substring(str.indexOf('href="') + 6, str.indexOf('/">') + 1);
                     var biturl = await bitly.shorten(hyperlink);
                     headline = headline + biturl.url;
+                   
+                    //print headline to console and push it onto array of headlines in that category.
                     console.log(headline);
                     categories[i].push(headline);
+                    
+                    //splice string to move onto the next html block headline.
                     str = str.substring(str.indexOf("<br /><br />") + 12);
                     endIndex = str.indexOf("href");
                 }
@@ -61,6 +95,7 @@ async function tweetFormatter() {
     return categories;
 }
 
+//call tweetFormatter and handle results to tweet headlines on an interval.
 tweetFormatter().then(function(result) {
     
 });
